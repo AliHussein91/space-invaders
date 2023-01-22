d54828a2d0e4212cf2b3e497ff8bd5fe2814cabd
Ali Hussein <ali.hussein@live.com>
2023-01-22 06:06:43 +0200

    Added Invaders Shooting

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class createPlayer {
+class Player {
   constructor() {
     this.velocity = {
       x: 0,
@@ -62,7 +62,7 @@ class projectile {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
-    this.radius = 3;
+    this.radius = 4;
   }
 
   draw() {
@@ -80,8 +80,88 @@ class projectile {
   }
 }
 
-const player = new createPlayer();
+class Invader {
+  constructor({ position }) {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    const image = new Image();
+    image.src = './img/invader.png';
+    image.onload = () => {
+      const scale = 1.2;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: position.x,
+        y: position.y,
+      };
+    };
+  }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update({ velocity }) {
+    if (this.image) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
+    }
+  }
+}
+
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+    this.velocity = {
+      x: 3,
+      y: 0,
+    };
+    this.invaders = [];
+    const columns = Math.floor(Math.random() * 10 + 5);
+    const rows = Math.floor(Math.random() * 5 + 2);
+
+    this.width = columns * 40;
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: x * 30,
+              y: y * 30,
+            },
+          })
+        );
+      }
+    }
+  }
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    this.velocity.y = 0;
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 30;
+    }
+  }
+}
+
+const player = new Player();
 const projectiles = [];
+const grids = [];
 const keys = {
   a: {
     pressed: false,
@@ -94,11 +174,16 @@ const keys = {
   },
 };
 
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 700 + 500);
+
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
+
   player.update();
+
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y + projectile.radius <= 0) {
       setTimeout(() => {
@@ -108,6 +193,37 @@ function animate() {
       projectile.update();
     }
   });
+
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader, i) => {
+      invader.update({ velocity: grid.velocity });
+      projectiles.forEach((projectile, p) => {
+        if (
+          projectile.position.y - projectile.radius <=
+            invader.position.y + invader.height &&
+          projectile.position.x + projectile.radius >= invader.position.x &&
+          projectile.position.x - projectile.radius <=
+            invader.position.x + invader.width &&
+          projectile.position.y + projectile.radius >= invader.position.y
+        ) {
+          setTimeout(() => {
+            const invaderFound = grid.invaders.find(
+              (invader2) => invader2 === invader
+            );
+            const projectileFound = projectiles.find(
+              (projectile2) => projectile2 === projectile
+            );
+            if (invaderFound && projectileFound) {
+              grid.invaders.splice(i, 1);
+              projectiles.splice(p, 1);
+            }
+          }, 0);
+        }
+      });
+    });
+  });
+
   if (keys.a.pressed && player.position.x > 0) {
     player.velocity.x = -10;
     player.rotation = -0.15;
@@ -121,6 +237,14 @@ function animate() {
     player.velocity.x = 0;
     player.rotation = 0;
   }
+
+  if (frames % randomInterval === 0) {
+    grids.push(new Grid());
+    randomInterval = Math.floor(Math.random() * 700 + 500);
+    frames = 0;
+  }
+
+  frames++;
 }
 
 animate();
